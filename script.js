e00436b0a896906a5b2b61344bd9913c85d3cbbc
Josue <jescamilla@aclanyc.com>
2017-07-07 19:21:59 +0000

    WIP

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
+    $('button').click(function(){
+        
+var answer = $("#pet").val();
+if( answer === "3"){
+    $(".h6").html("Yup!");
+}
+    else {
+$(".h6").html("Nope!");
+}
+    
 
+    });
 
+//MINESWEEPER
+var choice = ["Rock", "paper", "scissor"]
+$('.b').click(function(){
+   var num = Math.floor(Math.random()*2);
+   var answer = choice[num]
+    alert("The answer is" + answer)
+}
+)
 });
